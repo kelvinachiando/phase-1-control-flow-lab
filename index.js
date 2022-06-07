274704f670a04aabc0c19b547010ac3800cdb494
kelvinachiando <kelvinostin22gmail.com>
2022-06-07 22:33:16 +0300

    commit tree 996cb1995d39047b9abdd2367c05d06961d75a6f

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function switchOnCharmFromTip(tip){
   switch (tip){
     case "generous":
       return "Thank you so much";
-      break;
       case "not as generous":
       return "Thank you";
       case "anything else":
